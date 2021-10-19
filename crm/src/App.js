cef3342a0f2fd16885fe346084a531a8815fb616
Akarshit <kamra.akarshit99@gmail.com>
2021-10-19 15:32:36 +0530

    first commit

---
+++ crm/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./App.css";
+import "bootstrap/dist/js/bootstrap.min";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/navbar";
+
+function App() {
+  return <Navbar />;
+}
+
+export default App;
